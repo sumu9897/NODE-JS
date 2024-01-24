f76c238594451f08322bb6e953a5810cf9519fdc
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-24 16:56:57 +0600

    CRUD Operation

--- Source/index.js
+++ Source/index.js
@@ -17,6 +17,8 @@
 
 // Read File
 
+
+/*
 const http = require('http');
 const fs = require("fs");
 
@@ -27,4 +29,68 @@ http.createServer(function(req,res){
     })
 }).listen(4040,function(){
     console.log("Listening on port 4040");
+})
+
+*/
+
+
+//Crate File
+
+/*
+const http = require('http');
+const fs = require("fs");
+
+http.createServer(function(req,res){
+    fs.writeFile('demo1.text','This is 2nd Demo File',function(err){
+        if(err){
+            res.end("File Writing Fail");
+        }else{
+            res.end("File Written Successfully!");
+        }
+
+    })
+}).listen(4041,function(){
+    console.log("Listening on port 4041");
+})
+
+*/
+
+//Update File
+
+/*
+const http = require('http');
+const fs = require("fs");
+
+http.createServer(function(req,res){
+    fs.appendFile('demo1.text','Hello',function(err){
+        if(err){
+            res.end("File Update Fail");
+        }else{
+            res.end("File Update Successfully!");
+        }
+
+    })
+}).listen(4042,function(){
+    console.log("Listening on port 4042");
+})
+*/
+
+
+//Delete File
+
+
+const http = require('http');
+const fs = require("fs");
+
+http.createServer(function(req,res){
+    fs.unlink('demo1.text',function(err){
+        if(err){
+            res.end("File Delete Fail");
+        }else{
+            res.end("File Delete Successfully!");
+        }
+
+    })
+}).listen(4042,function(){
+    console.log("Listening on port 4042");
 })
