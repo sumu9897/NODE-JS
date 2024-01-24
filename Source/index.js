2f862a588afb8fc7626fa9abbf5c877739691791
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-24 16:33:20 +0600

    URL Module

--- Source/index.js
+++ Source/index.js
@@ -1,11 +1,30 @@
 //Working With URL Module
 
-const url=require('url') //ES5
+// const url=require('url') //ES5
 
-const myURL='http://localhost:8080/api/v1/product/delete?userID=1&productID=2'   //  Base+Path+Query
-const u= url.parse(myURL,true);
+// const myURL='http://localhost:8080/api/v1/product/delete?userID=1&productID=2'   //  Base+Path+Query
+// const u= url.parse(myURL,true);
 
 
-console.log(u.host);
-console.log(u.pathname);
-console.log(u.search);
+// console.log(u.host);
+// console.log(u.pathname);
+// console.log(u.search);
+
+
+
+
+
+
+// Read File
+
+const http = require('http');
+const fs = require("fs");
+
+http.createServer(function(req,res){
+    fs.readFile('demo.text', function(error,data){
+        res.write(data);
+        res.end();
+    })
+}).listen(4040,function(){
+    console.log("Listening on port 4040");
+})
