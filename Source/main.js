317a06da830b328cd3bc28488043440e2f2efa60
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-24 10:56:03 +0600

    Update

--- Source/main.js
+++ Source/main.js
@@ -2,6 +2,9 @@ const http=require('http');
 
 http.createServer(function(req,res){
 
+    //Basic
+    res.writeHead(201,{'Content-Type':'application/json'})
+    let person={name:"ABC",age:24}
     res.write("Hello I am from server");
     res.end();
 
