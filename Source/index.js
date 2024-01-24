7ae1117b975572e4f5b29137e7eb429e9292925a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-24 16:18:24 +0600

    URL Module

--- Source/index.js
+++ Source/index.js
@@ -1,3 +1,11 @@
 //Working With URL Module
 
-const url=require('url')
+const url=require('url') //ES5
+
+const myURL='http://localhost:8080/api/v1/product/delete?userID=1&productID=2'   //  Base+Path+Query
+const u= url.parse(myURL,true);
+
+
+console.log(u.host);
+console.log(u.pathname);
+console.log(u.search);
